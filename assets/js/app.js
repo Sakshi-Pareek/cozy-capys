e95109f6e0b1350324d3dc296cd2cafd71347ec6
sakshi pareek <sakshipareek505@gmail.com>
2023-09-29 13:58:35 +0530

    navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,10 @@
 
 let label = document.querySelector("#label")
 let navbox = document.querySelector(".navbox")
+let nav_bg = document.querySelector(".nav_bg")
 label.addEventListener("click", function () {
-    navbox.classList.toggle("right-0")
+    navbox.classList.toggle("right-0");
+    nav_bg.classList.toggle("z_index_5");
 })
 function showNav() {
     document.body.classList.toggle('overflow-hidden')
